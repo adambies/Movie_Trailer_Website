c9230ac3a1e0f271f9c563dcfa5be17078f22140
adambies <bies_adam@yahoo.com>
2017-09-13 14:20:21 -0600

    Add files via upload

--- enertainment_center.py
+++ enertainment_center.py
@@ -1,30 +1,49 @@
 import media
 import fresh_tomatoes
 
-# This is where the movie classes are instantiated.
+# The Hitman's Bodyguard movie: movie title, storyline, poster image and movie
+# trailer
+hitmans_bodyguard = media.Movie(
+    "The Hitman's Bodyguard", "A hitman hires a bodyguard to protect him.",
+    "https://upload.wikimedia.org/wikipedia/en/2/2d/HitmansBodyguard.jpg",
+    "https://www.youtube.com/watch?v=F4Afusxc2SM")
 
-hitmans_bodyguard = media.Movie("The Hitman's Bodyguard", "A hitman hires a bodyguard to protect him.",
-                                "https://upload.wikimedia.org/wikipedia/en/2/2d/HitmansBodyguard.jpg",
-                                "https://www.youtube.com/watch?v=F4Afusxc2SM")
+# It movie: movie title, storyline, poster image and movie trailer
+it = media.Movie(
+    "It", "A movie about a killer clown.",
+    "https://upload.wikimedia.org/wikipedia/en/a/a2/It_%282017%29_logo.jpg",
+    "https://www.youtube.com/watch?v=xKJmEC5ieOk"
+    )
+# School of Rock movie: movie title, storyline, poster image and movie trailer
+school_of_rock = media.Movie(
+    "School of Rock", "Using rock music to learn",
+    "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+    "https://www.youtube.com/watch?v=3PsUJFEBC74"
+    )
 
-it = media.Movie("It", "A movie about a killer clown.",
-                     "https://upload.wikimedia.org/wikipedia/en/a/a2/It_%282017%29_logo.jpg",
-                     "https://www.youtube.com/watch?v=xKJmEC5ieOk")
+# Ratatouille movie: movie title, storyline, poster image and movie trailer
+ratatouille = media.Movie(
+    "Ratatouille", "A rat is a chef in Paris",
+    "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk"
+    )
 
-school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
-
-ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris",
-                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
-
-midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors",
-                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+# Midnight in Paris movie: movie title, storyline, poster image and movie
+# trailer
+midnight_in_paris = media.Movie(
+    "Midnight in Paris", "Going back in time to meet authors",
+    "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=atLg2wQQxvU"
+    )
 
 # Create the list of movies to be used by the fresh tomatoes file
-movies = [hitmans_bodyguard, it, school_of_rock, ratatouille, midnight_in_paris]
+movies = [
+    hitmans_bodyguard,
+    it,
+    school_of_rock,
+    ratatouille,
+    midnight_in_paris
+    ]
 
-# Run the fresh tomatoes function to create the website
+# Open the HTML file in a web browser via the fresh_tomatoes.py file
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,17 +2,36 @@ import webbrowser
 
 
 class Movie(object):
+    """ This class provides a way to store movie related information."""
 
-    """ This class provides a way to store movie related information"""
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """This constructor creates space to store movie
+         information.
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+         Args:
+             movie_title (str): The title of the movie.
+             movie_storyline (str): Short description of movie's plot.
+             poster_image (str): Link to the poster image for the movie.
+             trailer_youtube (str): Link to the movie trailer on Youtube.
+
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """The show_trailer function opens a web browser and loads the trailer
+        from the url.
+        """
         webbrowser.open(self.trailer_youtube_url)
 
+
     def show_poster(self):
+        """The show poster function opens a web browser and loads the poster
+        image from the url.
+        """
         webbrowser.open(self.poster_image_url)
+
+
